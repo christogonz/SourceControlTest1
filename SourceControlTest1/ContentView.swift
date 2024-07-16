cf502068feadaccd7c4f75be5bb15cd2f5a07861
Chris Gonzalez <christogonz92@gmail.com>
2024-07-16 21:13:03 +0200

    [Feature] Remove Gonzalez fron text

--- SourceControlTest1/ContentView.swift
+++ SourceControlTest1/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "heart.fill")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Chris Gonzalez")
+            Text("Chris")
             
             Button("Subscribe") {
                 
