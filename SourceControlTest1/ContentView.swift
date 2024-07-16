60692b53f036fb9239e358c2d1508fa3d02a9d38
Chris Gonzalez <christogonz92@gmail.com>
2024-07-16 20:18:30 +0200

    [Clean] Add commit rules

--- SourceControlTest1/ContentView.swift
+++ SourceControlTest1/ContentView.swift
@@ -27,3 +27,25 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
+
+/*
+ Commit massages
+ 
+ NEW FEATURE:
+ [Feature] Description of the feature
+ 
+ BUG IN PRODUCTION:
+ [Patch] Description of patch
+ 
+ BUG NOT IN PRODUCTION
+ [Bug] description of the bug
+ 
+ MUNDANE TASKS:
+ [Clean] Description of changes
+ 
+ REALESE:
+ [Release] Description of release
+ 
+ */
+ 
