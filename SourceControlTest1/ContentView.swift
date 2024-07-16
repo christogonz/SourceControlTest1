a6002b73434090fe279cf8e9f8e900b09d9915af
Chris Gonzalez <christogonz92@gmail.com>
2024-07-16 19:13:45 +0200

    Initial Commit

---
+++ SourceControlTest1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SourceControlTest1
+//
+//  Created by Christopher Gonzalez on 2024-07-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SourceControlTest1/SourceControlTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlTest1App.swift
+//  SourceControlTest1
+//
+//  Created by Christopher Gonzalez on 2024-07-16.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
