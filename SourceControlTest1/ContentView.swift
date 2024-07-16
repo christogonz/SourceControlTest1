1944fb53286ea9981fc28b95defecd0a3c5555e9
Chris Gonzalez <christogonz92@gmail.com>
2024-07-16 21:08:03 +0200

    [Feature] Rename the subscribe button.

--- SourceControlTest1/ContentView.swift
+++ SourceControlTest1/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Chris Gonzalez")
             
-            Button("Subscribe now!") {
+            Button("Subscribe") {
                 
             }
             
