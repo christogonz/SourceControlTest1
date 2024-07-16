90319cd4deb83515b8d5ccfc3e3536b24f370115
Chris Gonzalez <christogonz92@gmail.com>
2024-07-16 19:49:54 +0200

    My first commit

--- SourceControlTest1/ContentView.swift
+++ SourceControlTest1/ContentView.swift
@@ -10,11 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "heart.fill")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Chris Gonzalez")
         }
+        .padding()
         .padding()
     }
 }
