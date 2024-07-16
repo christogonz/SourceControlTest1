3f2a9deaadbc3f258fed3d6b18e57f85806cb604
Chris Gonzalez <christogonz92@gmail.com>
2024-07-16 20:01:12 +0200

    Add HomeView

---
+++ SourceControlTest1/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SourceControlTest1
+//
+//  Created by Christopher Gonzalez on 2024-07-16.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2")
+    }
+}
+
+#Preview {
+    HomeView()
+}
