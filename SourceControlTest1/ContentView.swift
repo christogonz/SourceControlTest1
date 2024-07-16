add7cac897ade5721816451e5fd243174b66e45c
Chris Gonzalez <christogonz92@gmail.com>
2024-07-16 20:45:09 +0200

    [Feature] Update Subscribe button

--- SourceControlTest1/ContentView.swift
+++ SourceControlTest1/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Chris Gonzalez")
             
-            Button("Click me") {
+            Button("Subscribe now!") {
                 
             }
             
