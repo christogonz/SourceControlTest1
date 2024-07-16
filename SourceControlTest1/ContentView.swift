3c83a200b8aadc036aed3cdb5b26796d1a843510
Chris Gonzalez <christogonz92@gmail.com>
2024-07-16 20:35:29 +0200

    [Feature] Add Rectangle to ContentView

--- SourceControlTest1/ContentView.swift
+++ SourceControlTest1/ContentView.swift
@@ -18,6 +18,8 @@ struct ContentView: View {
             Button("Click me") {
                 
             }
+            
+            Rectangle()
         }
         .padding()
         .padding()
