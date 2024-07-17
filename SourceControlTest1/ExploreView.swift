1cf23b11dd55e6c84dc460a7cef77eb805c5f684
Chris Gonzalez <christogonz92@gmail.com>
2024-07-17 13:19:28 +0200

    [Feature] Add a ExploreView

---
+++ SourceControlTest1/ExploreView.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreView.swift
+//  SourceControlTest1
+//
+//  Created by Christopher Gonzalez on 2024-07-17.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExploreView()
+}
